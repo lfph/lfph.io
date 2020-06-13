dce2081207013f0359fe724f5dc1074cba06b7aa
cjyabraham <cjyabraham@gmail.com>
2020-06-13 13:28:16 +0800

    dropped in fresh cncf fork

--- web/wp-content/mu-plugins/wp-no-base-permalink.php
+++ web/wp-content/mu-plugins/wp-no-base-permalink.php
@@ -4,7 +4,7 @@
  * Description: Removes category base or tag base (optional) from your category or tag permalinks and removes parents categories from your category permalinks (optional). Forked from https://wordpress.org/plugins/wp-no-base-permalink/ by Sergio ( kallookoo ).
  * Version: 9.9
  * Author: James Hunt
- * Author URI: https://lfph.io
+ * Author URI: https://cncf.io
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: wp-no-base-permalink
---
+++ web/wp-content/themes/lf-theme/includes/image-sizes.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Image sizes
+ *
+ * Used to create new image sizes used in the site. TRUE uses hard crop to size.
+ *
+ * @category Components
+ * @package  WordPress
+ * @author   James Hunt <domains@thetwopercent.co.uk>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link     https://cncf.io
+ * @since    1.0.0
+ */
+
+// add_image_size('icon', 50, 50, true); // phpcs:ignore.
+// add_image_size('new-size', 215, 215, true); // phpcs:ignore.
+
+add_image_size( 'people', 100, 100, true );
+add_image_size( 'newsroom-media-coverage', 260, 160, false );
+add_image_size( 'newsroom-image-small', 260, 160, true );
+add_image_size( 'newsroom-image', 520, 320, true );
+add_image_size( 'newsroom-image-large', 1040, 640, true );
+add_image_size( 'spotlight', 285, 150, false );
+add_image_size( 'case-study', 315, 280, true );
