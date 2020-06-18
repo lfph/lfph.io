ce147853ae82a6b323191372eb253744ff643eb0
James Hunt <jim@thetwopercent.co.uk>
2020-06-18 17:02:12 +0100

    Update image sizes, update hero dimensions for FOUC

--- web/wp-content/themes/lf-theme/includes/image-sizes.php
+++ web/wp-content/themes/lf-theme/includes/image-sizes.php
@@ -15,10 +15,20 @@
 // add_image_size('icon', 50, 50, true); // phpcs:ignore.
 // add_image_size('new-size', 215, 215, true); // phpcs:ignore.
 
-add_image_size( 'people', 100, 100, true );
+// Press images, no hard crop.
 add_image_size( 'newsroom-media-coverage', 260, 160, false );
+
+// Newsroom, mobile, featured, retina.
 add_image_size( 'newsroom-image-small', 260, 160, true );
-add_image_size( 'newsroom-image', 520, 320, true );
-add_image_size( 'newsroom-image-large', 1040, 640, true );
-add_image_size( 'spotlight', 285, 150, false );
-add_image_size( 'case-study', 315, 280, true );
+add_image_size( 'newsroom-image', 600, 320, true );
+add_image_size( 'newsroom-image-large', 1200, 640, true );
+
+// additional sections.
+add_image_size( 'spotlight', 320, 170, false );
+add_image_size( 'case-study', 320, 260, true );
+
+// Hero image.
+add_image_size( 'hero-extra-large', 1920, 260, true );
+add_image_size( 'hero-large', 1400, 260, true );
+add_image_size( 'hero-medium', 1000, 220, true );
+add_image_size( 'hero-small', 414, 220, true );
