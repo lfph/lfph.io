c4f3b8e139dd29accca9348f540a72dd7cded953
James Hunt <jim@thetwopercent.co.uk>
2020-06-23 14:06:48 +0100

    Adding organisation to newsletter, closes #17

--- web/wp-content/themes/lf-theme/components/newsletter.php
+++ web/wp-content/themes/lf-theme/components/newsletter.php
@@ -15,26 +15,39 @@
 	<form id="sfmc-form1" class="newsletter-form"
 		action="https://cloud.email.thelinuxfoundation.org/LFPH-Newsletter-Form-Submission">
 		<label for="FirstName" required>
-		<span class="screen-reader-text">First Name</span>
-			<input type="text" id="FirstName" name="FirstName" placeholder="First Name" autocomplete="given-name" spellcheck="false"
-				required>
+			<span class="screen-reader-text">First Name</span>
+			<input type="text" id="FirstName" name="FirstName"
+				placeholder="First Name" autocomplete="given-name"
+				spellcheck="false" required>
 		</label>
 		<label for="LastName" required>
-		<span class="screen-reader-text">Last Name</span>
-			<input type="text" id="LastName" name="LastName" placeholder="Last Name" autocomplete="family-name" spellcheck="false"
-				required>
+			<span class="screen-reader-text">Last Name</span>
+			<input type="text" id="LastName" name="LastName"
+				placeholder="Last Name" autocomplete="family-name"
+				spellcheck="false" required>
+		</label>
+		<label for="Organization">
+			<span class="screen-reader-text">Organization</span>
+			<input type="text" id="Organization" name="Organization"
+				placeholder="Organization"
+				autcomplete="org"
+				spellcheck="false">
 		</label>
 		<label for="EmailAddress" required>
-		<span class="screen-reader-text">Email Address</span>
+			<span class="screen-reader-text">Email Address</span>
 			<input type="email" id="EmailAddress" name="EmailAddress"
-				placeholder="Email Address" autocomplete="email" spellcheck="false" required>
+				placeholder="Email Address" autocomplete="email"
+				spellcheck="false" required>
 		</label>
-		<button type="submit" class="button stocky" id="sfmc-submit1">Subscribe</button>
+		<button type="submit" class="button stocky"
+			id="sfmc-submit1">Subscribe</button>
 		<div id="recaptcha-form1" style="display:none;"></div>
 	</form>
 	<div id="sfmc-message1" class="form-message"></div>
-	<p class="privacy-agreement">By submitting this form, you acknowledge that your
+	<p class="privacy-agreement">By submitting this form, you acknowledge that
+		your
 		information is subject to The Linux Foundation’s <a
 			href="https://www.linuxfoundation.org/privacy/"
-			rel="norefferer noopener" class="external is-footer" target="_blank">Privacy Policy</a>.</p>
+			rel="norefferer noopener" class="external is-footer"
+			target="_blank">Privacy Policy</a>.</p>
 </section>
