e5b4ae879f264f8a201192e4e32f6b40040a04f5
cjyabraham <cjyabraham@gmail.com>
2020-07-20 09:01:33 +0800

    copy change

--- web/wp-content/themes/lf-theme/components/wip-front-page-hero.php
+++ web/wp-content/themes/lf-theme/components/wip-front-page-hero.php
@@ -16,7 +16,7 @@
 		</figure>
 
 		<div class="background-image-text-overlay">
-			<h1>Collaborating to<br/> combat COVID-19</h1>
+			<h1>Collaborating to<br/> battle COVID-19</h1>
 			<p>Linux Foundation Public Health (LFPH) builds, secures and sustains open source software to help public health authorities (PHAs) combat COVID-19 and future epidemics.
 			</p>
 
