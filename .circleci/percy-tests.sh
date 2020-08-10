3e5f2344db22d6279bf5ed97c9d925f80723c88d
Chris Abraham <cjyabraham@gmail.com>
2020-08-10 11:06:30 +0800

    Add Percy for visual regression testing; remove wraith (#84)
    * strip out wraith stuff and add percy stuff
    
    * configure percy
    
    * set defaults for percy
    
    * update config
    
    * try testing no matter what branch
    
    * need to install percy script
    
    * speed up this testing
    
    * try two tests
    
    * ready to merge

---
+++ .circleci/percy-tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TERMINUS_S=$1
+echo 'export PERCY_TOKEN=${PERCY_TOKEN}' >> $BASH_ENV
+source $BASH_ENV
+
+if [[ (${CIRCLE_BRANCH} != "master") || (${CIRCLE_BRANCH} == "master" && -n ${CIRCLE_PULL_REQUEST+x}) ]];
+then
+    echo -e "CircleCI will only run tests on Pantheon if on the master branch.\n"
+    exit 0;
+fi
+
+# Exit immediately on errors
+set -ex
+
+# Install Percy
+npm install -D @percy/script
+
+# Run the tests
+npx percy exec -- node ./percy/percy.js
---
+++ percy/percy.js
@@ -0,0 +1,32 @@
+const PercyScript = require('@percy/script');
+let rooturl = 'https://dev-lfph.pantheonsite.io/';
+
+PercyScript.run(async (page, percySnapshot) => {
+  await page.goto(rooturl);
+  await page.waitFor('.newsletter');
+  await percySnapshot('homepage');
+
+  await page.goto(rooturl + 'projects/');
+  await page.waitFor('.newsletter');
+  await percySnapshot('projects');
+
+  await page.goto(rooturl + 'join/faq/#what-is-upstream-what-is-a-fork-what-is-a-maintainer');
+  await page.waitFor(1000);
+  await percySnapshot('faq at anchor');
+
+  await page.goto(rooturl + 'community/webinars/');
+  await page.waitFor('.newsletter');
+  await percySnapshot('webinars');
+
+  await page.goto(rooturl + 'blog/');
+  await page.waitFor('.newsletter');
+  await percySnapshot('blog');
+
+  await page.goto(rooturl + 'about/staff/');
+  await page.waitFor('.newsletter');
+  await percySnapshot('staff');
+
+  await page.goto(rooturl + 'about/contact/');
+  await page.waitFor('.newsletter');
+  await percySnapshot('contact');
+});
