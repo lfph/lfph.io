348a46933d4a8824619ddf72a11d3a8648566574
Chris Abraham <cjyabraham@gmail.com>
2020-08-10 14:43:13 +0800

    added !!! (#85)

--- web/wp-content/themes/lf-theme/components/wip-front-page-hero.php
+++ web/wp-content/themes/lf-theme/components/wip-front-page-hero.php
@@ -17,7 +17,7 @@
 
 		<div class="background-image-text-overlay">
 			<h1>Collaborating to <br/>battle COVID-19</h1>
-			<p class="h4">Linux Foundation Public Health (LFPH) builds, secures, and sustains open source software to help public health authorities (PHAs) combat COVID-19 and future epidemics.
+			<p class="h4">Linux Foundation Public Health (LFPH) builds, secures, and sustains open source software to help public health authorities (PHAs) combat COVID-19 and future epidemics!!!
 			</p>
 
 		</div>
