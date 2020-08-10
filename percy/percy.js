4c0fe8408232381e31c56d8eecd2170db3e8a0f5
cjyabraham <cjyabraham@gmail.com>
2020-08-10 15:40:32 +0800

    wait 1s prior to snapshots to make sure page is fully rendered

--- percy/percy.js
+++ percy/percy.js
@@ -3,11 +3,11 @@ let rooturl = process.argv[2];
 
 PercyScript.run(async (page, percySnapshot) => {
   await page.goto(rooturl);
-  await page.waitFor('.newsletter');
+  await page.waitFor(1000);
   await percySnapshot('homepage');
 
   await page.goto(rooturl + 'projects/');
-  await page.waitFor('.newsletter');
+  await page.waitFor(1000);
   await percySnapshot('projects');
 
   await page.goto(rooturl + 'join/faq/#what-is-upstream-what-is-a-fork-what-is-a-maintainer');
@@ -15,18 +15,18 @@ PercyScript.run(async (page, percySnapshot) => {
   await percySnapshot('faq at anchor');
 
   await page.goto(rooturl + 'community/webinars/');
-  await page.waitFor('.newsletter');
+  await page.waitFor(1000);
   await percySnapshot('webinars');
 
   await page.goto(rooturl + 'blog/');
-  await page.waitFor('.newsletter');
+  await page.waitFor(1000);
   await percySnapshot('blog');
 
   await page.goto(rooturl + 'about/staff/');
-  await page.waitFor('.newsletter');
+  await page.waitFor(1000);
   await percySnapshot('staff');
 
   await page.goto(rooturl + 'about/contact/');
-  await page.waitFor('.newsletter');
+  await page.waitFor(1000);
   await percySnapshot('contact');
 });
