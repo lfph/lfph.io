3ea4fb6617a8ecfe8efb9f75d19959eed73d803a
cjyabraham <cjyabraham@gmail.com>
2020-08-10 11:05:36 +0800

    ready to merge

--- .circleci/percy-tests.sh
+++ .circleci/percy-tests.sh
@@ -4,11 +4,11 @@ TERMINUS_S=$1
 echo 'export PERCY_TOKEN=${PERCY_TOKEN}' >> $BASH_ENV
 source $BASH_ENV
 
-# if [[ (${CIRCLE_BRANCH} != "master") || (${CIRCLE_BRANCH} == "master" && -n ${CIRCLE_PULL_REQUEST+x}) ]];
-# then
-#     echo -e "CircleCI will only run tests on Pantheon if on the master branch.\n"
-#     exit 0;
-# fi
+if [[ (${CIRCLE_BRANCH} != "master") || (${CIRCLE_BRANCH} == "master" && -n ${CIRCLE_PULL_REQUEST+x}) ]];
+then
+    echo -e "CircleCI will only run tests on Pantheon if on the master branch.\n"
+    exit 0;
+fi
 
 # Exit immediately on errors
 set -ex
--- percy/percy.js
+++ percy/percy.js
@@ -10,4 +10,23 @@ PercyScript.run(async (page, percySnapshot) => {
   await page.waitFor('.newsletter');
   await percySnapshot('projects');
 
+  await page.goto(rooturl + 'join/faq/#what-is-upstream-what-is-a-fork-what-is-a-maintainer');
+  await page.waitFor(1000);
+  await percySnapshot('faq at anchor');
+
+  await page.goto(rooturl + 'community/webinars/');
+  await page.waitFor('.newsletter');
+  await percySnapshot('webinars');
+
+  await page.goto(rooturl + 'blog/');
+  await page.waitFor('.newsletter');
+  await percySnapshot('blog');
+
+  await page.goto(rooturl + 'about/staff/');
+  await page.waitFor('.newsletter');
+  await percySnapshot('staff');
+
+  await page.goto(rooturl + 'about/contact/');
+  await page.waitFor('.newsletter');
+  await percySnapshot('contact');
 });
