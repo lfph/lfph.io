6f84d8f16bc8dcf006360a93d89de33134cef04d
cjyabraham <cjyabraham@gmail.com>
2020-08-10 09:40:15 +0800

    configure percy

--- percy/percy.js
+++ percy/percy.js
@@ -1,7 +1,8 @@
 const PercyScript = require('@percy/script');
+let rooturl = 'https://dev-lfph.pantheonsite.io/';
 
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('https://dev-lfph.pantheonsite.io/');
+  await page.goto(rooturl);
   // ensure the page has loaded before capturing a snapshot
   await page.waitFor('.newsletter');
   await percySnapshot('homepage');
