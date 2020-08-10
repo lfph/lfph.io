5fc3a8cebbb805ec49cfc612f4521685dfef400c
cjyabraham <cjyabraham@gmail.com>
2020-08-10 11:00:00 +0800

    try two tests

--- percy/percy.js
+++ percy/percy.js
@@ -3,7 +3,11 @@ let rooturl = 'https://dev-lfph.pantheonsite.io/';
 
 PercyScript.run(async (page, percySnapshot) => {
   await page.goto(rooturl);
-  // ensure the page has loaded before capturing a snapshot
   await page.waitFor('.newsletter');
   await percySnapshot('homepage');
+
+  await page.goto(rooturl + 'projects/');
+  await page.waitFor('.newsletter');
+  await percySnapshot('projects');
+
 });
