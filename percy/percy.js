40e7f91db5d3a7b454c8bbc6d6fd41ec68406450
cjyabraham <cjyabraham@gmail.com>
2020-08-10 16:49:17 +0800

    increase delay before grabbing snapshot

--- percy/percy.js
+++ percy/percy.js
@@ -1,32 +1,33 @@
 const PercyScript = require('@percy/script');
 let rooturl = process.argv[2];
+let delay = 5000;
 
 PercyScript.run(async (page, percySnapshot) => {
   await page.goto(rooturl);
-  await page.waitFor(1000);
+  await page.waitFor(delay);
   await percySnapshot('homepage');
 
   await page.goto(rooturl + 'projects/');
-  await page.waitFor(1000);
+  await page.waitFor(delay);
   await percySnapshot('projects');
 
   await page.goto(rooturl + 'join/faq/#what-is-upstream-what-is-a-fork-what-is-a-maintainer');
-  await page.waitFor(1000);
+  await page.waitFor(delay);
   await percySnapshot('faq at anchor');
 
   await page.goto(rooturl + 'community/webinars/');
-  await page.waitFor(1000);
+  await page.waitFor(delay);
   await percySnapshot('webinars');
 
   await page.goto(rooturl + 'blog/');
-  await page.waitFor(1000);
+  await page.waitFor(delay);
   await percySnapshot('blog');
 
   await page.goto(rooturl + 'about/staff/');
-  await page.waitFor(1000);
+  await page.waitFor(delay);
   await percySnapshot('staff');
 
   await page.goto(rooturl + 'about/contact/');
-  await page.waitFor(1000);
+  await page.waitFor(delay);
   await percySnapshot('contact');
 });
