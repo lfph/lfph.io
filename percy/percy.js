1ebdcfe15df15ae0b9a4165fdf23b3fec77a64e8
cjyabraham <cjyabraham@gmail.com>
2020-08-11 10:12:24 +0800

    try a few more waits

--- percy/percy.js
+++ percy/percy.js
@@ -6,8 +6,9 @@ PercyScript.run(async (page, percySnapshot) => {
   await page.goto(rooturl);
   await page.waitFor(delay);
   await percySnapshot('homepage');
+  await page.waitFor(2000);
   await page.evaluate(()=>document.querySelector('button.search-button').click());
-  await page.waitFor('.search-input');
+  await page.waitFor(2000);
   await percySnapshot('homepage w search bar');
 
   await page.goto(rooturl + 'projects/');
