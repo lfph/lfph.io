d96ea731c452be4a2df0c1874bde9a7c8e8fe184
Chris Abraham <cjyabraham@gmail.com>
2021-03-01 16:31:09 +0800

    HubSpot (#99)
    * setup HubSpot newsletter form in footer
    
    * removed contact form shortcode

--- web/wp-content/themes/lf-theme/functions.php
+++ web/wp-content/themes/lf-theme/functions.php
@@ -81,9 +81,6 @@ require_once 'includes/shortcode-people.php';
 // projects shortcode.
 require_once 'includes/shortcode-projects.php';
 
-// contact form shortcode.
-require_once 'includes/shortcode-contact.php';
-
 // Fuerza utils.
 require_once 'classes/class-fuerza-utils.php';
 
