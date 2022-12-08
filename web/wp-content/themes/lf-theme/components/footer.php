39558ddd6ddc98d37bd7c186378595fb195c4371
rs59 <97830491+rs59@users.noreply.github.com>
2022-12-08 13:21:11 +0100

    Footer updates
    Removed newsletter signup and submit issue with page
    
    Signed-off-by: rs59 <97830491+rs59@users.noreply.github.com>

--- web/wp-content/themes/lf-theme/components/footer.php
+++ web/wp-content/themes/lf-theme/components/footer.php
@@ -15,14 +15,14 @@ $options = get_option( 'lf-mu' );
 <footer class="footer">
 	<div class="container wrap">
 
-	<?php get_template_part( 'components/newsletter' ); ?>
+/* 	<?php get_template_part( 'components/newsletter' ); ?> */ // Removed while putting webiste into deep freeze mode
 
 	<div class="copyright-text">
 				<p>Copyright &copy; <?php echo esc_html( gmdate( 'Y' ) ); ?>
 					<?php echo wp_kses_post( $options['copyright_textarea'] ); ?>
 				</p>
 
-				<p><a target="_blank" class="external" rel="noopener" href="https://github.com/lfph/lfph.io/issues/new?title=Your+issue&projects=lfph/lfph.io/1&body=From+URL%3A+<?php echo urlencode( get_permalink() ); ?>">Submit issue with this page on GitHub</a>.</p>
+				/* <p><a target="_blank" class="external" rel="noopener" href="https://github.com/lfph/lfph.io/issues/new?title=Your+issue&projects=lfph/lfph.io/1&body=From+URL%3A+<?php echo urlencode( get_permalink() ); ?>">Submit issue with this page on GitHub</a>.</p>  */ // Removed while putting webiste into deep freeze mode
 			</div>
 
 	<?php get_template_part( 'components/social-links' ); ?>
