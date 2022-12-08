01d70b215d68ade212f451c570a37b3051c93309
rs59 <97830491+rs59@users.noreply.github.com>
2022-12-08 12:43:04 +0100

    Removed newsletter signup
    Signed-off-by: rs59 <97830491+rs59@users.noreply.github.com>

--- web/wp-content/themes/lf-theme/components/newsletter.php
+++ web/wp-content/themes/lf-theme/components/newsletter.php
@@ -8,11 +8,3 @@
  */
 
 ?>
-<section class="newsletter" id="newsletter">
-	<h3>Subscribe to the LFPH newsletter to receive regular updates about
-		webinars, events & latest news</h3>
-
-	<?php echo do_shortcode( '[hubspot type=form portal=8112310 id=fbf5c157-5cef-4a9c-8f4e-db71bf3f252f]' ); ?>
-	<p class="privacy-agreement">By submitting this form, you acknowledge that your information is subject to The Linux Foundation’s <a href="https://www.linuxfoundation.org/privacy/" rel="noopener" class="external"
-target="_blank">Privacy Policy</a>.</p>
-</section>
