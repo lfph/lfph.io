0cd9d771a08282a6dc27f455369554ab240378de
rs59 <97830491+rs59@users.noreply.github.com>
2022-12-08 13:23:10 +0100

    Update footer.php
    Rearranged comments for visibility
    
    Signed-off-by: rs59 <97830491+rs59@users.noreply.github.com>

--- web/wp-content/themes/lf-theme/components/footer.php
+++ web/wp-content/themes/lf-theme/components/footer.php
@@ -15,14 +15,16 @@ $options = get_option( 'lf-mu' );
 <footer class="footer">
 	<div class="container wrap">
 
-/* 	<?php get_template_part( 'components/newsletter' ); ?> */ // Removed while putting webiste into deep freeze mode
+	// Removed while putting website into deep freeze mode
+	// <?php get_template_part( 'components/newsletter' ); ?> 
 
 	<div class="copyright-text">
 				<p>Copyright &copy; <?php echo esc_html( gmdate( 'Y' ) ); ?>
 					<?php echo wp_kses_post( $options['copyright_textarea'] ); ?>
 				</p>
 
-				/* <p><a target="_blank" class="external" rel="noopener" href="https://github.com/lfph/lfph.io/issues/new?title=Your+issue&projects=lfph/lfph.io/1&body=From+URL%3A+<?php echo urlencode( get_permalink() ); ?>">Submit issue with this page on GitHub</a>.</p>  */ // Removed while putting webiste into deep freeze mode
+				// Removed while putting website into deep freeze mode
+				// <p><a target="_blank" class="external" rel="noopener" href="https://github.com/lfph/lfph.io/issues/new?title=Your+issue&projects=lfph/lfph.io/1&body=From+URL%3A+<?php echo urlencode( get_permalink() ); ?>">Submit issue with this page on GitHub</a>.</p>
 			</div>
 
 	<?php get_template_part( 'components/social-links' ); ?>
